76c6ba27d52cab3ebffcf20c8ea862420e8d7382
Jeff Lyon <jeff@albatrossdigital.com>
2015-03-05 12:33:05 -0800

    Restructuring angular_media, adding placeholder field style

--- sites/all/modules/angular_media/templates/app.tpl.php
+++ sites/all/modules/angular_media/templates/app.tpl.php
@@ -1,40 +1,50 @@
 <script type="text/javascript">var appUrl = '<?php print $app_url; ?>';</script>
 <div class="angular-media-field" <?php if($hidden): ?>style="display: none;"<?php endif; ?>>
   <div ui-view></div>
-  <div class="thumbnails" ng-class="" ng-controller="thumbnails" ng-init='init({
-  apiUrl: "<?php print $api_url; ?>",
-  fieldName: "<?php print $field_name; ?>",
-  files: <?php print drupal_json_encode($files, JSON_HEX_APOS); ?>,
-  cardinality: <?php print (int)$cardinality; ?>,
-  tabs: <?php print $tabs; ?>,
-  allowedTypes: <?php print $allowed_types; ?>,
-  cropRatio: "<?php print $crop_ratio; ?>",
-  allowedSchemes: <?php print $allowed_schemes; ?>,
-  flickrApiKey: "<?php print $flickr_key; ?>",
-  addlFieldName: "<?php print $addl_field_name; ?>",
-})'><!-- nv-file-drop="" uploader="uploader"-->
+  <div class="thumbnails" ng-class="" ng-controller="thumbnails" ng-init="init(<?php print str_replace('"', '\'', $settings); ?>)"><!-- nv-file-drop="" uploader="uploader"-->
     
     
     <?php if(!$hidden): ?>
-      <div class="thumbnail" ng-repeat="(key, file) in files['<?php print $field_name; ?>']" title="{{file.name}}">
-        <div thumbnails></div>
-        <a href class="thumbnail-title" ng-click="edit(file.fid)">{{file.name}}</a>
-          <button type="button" class="btn btn-default btn-xs thumbnail-edit" ng-click="edit(file.fid, '<?php print $field_name; ?>', key)"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></button>
-          <button type="button" class="btn btn-danger btn-xs thumbnail-remove" ng-click="remove('<?php print $field_name; ?>', key)"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
-      </div>
-      <div ng-controller="upload">
-        <div ng-class="{'upload-active': uploading}" class="thumbnail drop-zone" nv-file-drop="" uploader="uploader">
-          <div><span class="glyphicon glyphicon-download" aria-hidden="true"></span> Drop files</div>
-          <div class="or">or</div>
-          <button class="btn btn-primary media-select" id="<?php print $field_name; ?>_select" ng-click="select('<?php print $field_name; ?>', $event)"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</button>
+
+      <?php if($style == 'placeholder'): ?>
+        <div class="placeholder-thumbnail" ng-repeat="(key, file) in files['<?php print $fieldName; ?>']" title="{{file.name}}">
+          <img ng-if="file.thumbUrl" ng-src="{{file.url}}" />
+          <button type="button" class="btn btn-default btn-action thumbnail-edit" ng-click="edit(file.fid, '<?php print $fieldName; ?>', key)"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></button>
+          <button type="button" class="btn btn-danger btn-action thumbnail-remove" ng-click="remove('<?php print $fieldName; ?>', key)"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
         </div>
-      </div>
+        <div ng-controller="upload" ng-if="!files['<?php print $fieldName; ?>'].length">
+          <div ng-class="{'upload-active': uploading}" class="placeholder-thumbnail drop-zone" nv-file-drop="" uploader="uploader" ng-click="select('<?php print $fieldName; ?>', $event)">
+            <p class="upload-text">Drop an image or select a file to bring your content to life</p>
+            <button class="btn btn-primary media-select" id="<?php print $fieldName; ?>_select" ng-click="select('<?php print $fieldName; ?>', $event)"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</button>
+          </div>
+        </div>
+
+      <?php else: ?>
+
+        <div class="thumbnail" ng-repeat="(key, file) in files['<?php print $fieldName; ?>']" title="{{file.name}}">
+          <div thumbnails></div>
+          <a href class="thumbnail-title" ng-click="edit(file.fid)">{{file.name}}</a>
+            <button type="button" class="btn btn-action btn-default btn-xs thumbnail-edit" ng-click="edit(file.fid, '<?php print $fieldName; ?>', key)"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></button>
+            <button type="button" class="btn btn-action btn-danger btn-xs thumbnail-remove" ng-click="remove('<?php print $fieldName; ?>', key)"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
+        </div>
+        <div ng-controller="upload">
+          <div ng-class="{'upload-active': uploading}" class="thumbnail drop-zone" nv-file-drop="" uploader="uploader">
+            <div><span class="glyphicon glyphicon-download" aria-hidden="true"></span> Drop files</div>
+            <div class="or">or</div>
+            <button class="btn btn-primary media-select" id="<?php print $fieldName; ?>_select" ng-click="select('<?php print $fieldName; ?>', $event)"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</button>
+          </div>
+        </div>
+
+      <?php endif; ?>
+
     <?php else: ?>
-      <button class="btn btn-primary media-select" id="<?php print $field_name; ?>_select" ng-click="select('<?php print $field_name; ?>', $event)"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</button>
+
+      <button class="btn btn-primary media-select" id="<?php print $fieldName; ?>_select" ng-click="select('<?php print $fieldName; ?>', $event)"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</button>
+    
     <?php endif; ?>
   </div>
 
-  <input type="hidden" name="<?php print $field_name; ?>_media" id="<?php print $field_name; ?>_media" value="{{files['<?php print $field_name; ?>']}}" />
+  <input type="hidden" name="<?php print $fieldName; ?>_media" id="<?php print $fieldName; ?>_media" value="{{files['<?php print $fieldName; ?>']}}" />
   
   <div class="clearfix"></div>
 </div> 
