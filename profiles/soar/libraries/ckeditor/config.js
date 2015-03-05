11ab453c0b2a9e51c0d25411eb9ea8cb160bd742
Jeff Lyon <jeff@albatrossdigital.com>
2015-03-05 12:35:14 -0800

    Add autogrow to ckeditor default config

--- profiles/soar/libraries/ckeditor/config.js
+++ profiles/soar/libraries/ckeditor/config.js
@@ -35,5 +35,5 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-	config.extraPlugins = 'widgetbootstrap,widgettemplatemenu';
+	config.extraPlugins = 'widgetbootstrap,widgettemplatemenu,autogrow';
 };
