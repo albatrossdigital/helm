8173b5e4612cc7d91b5f1c309b31060b0721d6a8
Jeff Lyon <jeff@albatrossdigital.com>
2015-03-17 00:16:21 -0700

    Initial commit

--- profiles/helm/modules/contrib/angular_media/js/angular-media.js
+++
@@ -1,8 +0,0 @@
-
-(function($, Drupal) {
-  Drupal.behaviors.angular_media = {
-    attach: function(context, settings) {
-      angular.bootstrap(context, ['app']);
-    }
-  };
-})(jQuery, Drupal);
--- profiles/helm/modules/contrib/apps/theme/apps-update-page.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php if (!empty($content['apps'])) : ?>
-  <div id="apps-list">
-    <?php print drupal_render($content['apps']); ?>
-  </div>
-<?php else: ?>
-  <?php print t('No apps with available updates.'); ?>
-<?php endif; ?>
