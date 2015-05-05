f1a67b6282f3464914ea4fec483072a30c8e0b47
Alex Schmoe <aschmoe@gmail.com>
2015-05-04 21:23:54 -0700

    fixing app tpl

--- sites/all/modules/custom/angular_311/templates/app.tpl.php
+++ sites/all/modules/custom/angular_311/templates/app.tpl.php
@@ -1,10 +1,10 @@
 <div id="<?php print $app_id; ?>" class="app-wrap">
-  <div class="open-nav">
+  <div ng-if="appPageDisplay == 'all'" class="open-nav">
     <a menu-click="return-false"><i class="fa fa-arrow-circle-o-left"></i> <span>Navigation</span></a>
   </div>
   <div class="app-slider">
     <div class="row">
-      <div class="col-xs-6 col-sm-12 nav-contain">
+      <div ng-if="appPageDisplay == 'all'" class="col-xs-6 col-sm-12 nav-contain">
         <div menu></div>
       </div>
 
