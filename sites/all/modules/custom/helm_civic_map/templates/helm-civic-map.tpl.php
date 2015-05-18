429ba6d05c0ca044bfff3df63474b42202903906
Jeff Lyon <jeff@albatrossdigital.com>
2015-05-17 18:58:59 -0700

    Updating explore title text

--- sites/all/modules/custom/helm_civic_map/templates/helm-civic-map.tpl.php
+++ sites/all/modules/custom/helm_civic_map/templates/helm-civic-map.tpl.php
@@ -1,5 +1,5 @@
 <div id="helm-map-wrapper">
-  <h3 class="helm-map-title">Explore Springfield</h3>
+  <h3 class="helm-map-title">Explore</h3>
   <nav id="menu-ui" class="menu-ui"></nav>
   <div class="helm-map" id="helm-map"></div>
 </div>
