49a6fda21f5841707d1f73bc21bcdda0361a4741
Alex Schmoe <aschmoe@gmail.com>
2015-07-08 15:11:01 -0700

    updating dynamic sidebar element

--- profiles/helm/modules/features/helm_page/modules/helm_page_display/js/soar-dynamic-menu.js
+++ profiles/helm/modules/features/helm_page/modules/helm_page_display/js/soar-dynamic-menu.js
@@ -4,15 +4,22 @@
 Drupal.behaviors.facetapi = {
   attach: function(context, settings) {
 
-    $('[data-menu-back]').click(function(e) {
-      var $self = $(this),
-        level = $self.data('menu-back');
+    function menuClick($self, selector, e) {
+      var level = $self.data(selector);
       // switch classes
       $self.closest('.menu-block-wrapper')
            .addClass('level-' + level + '-active')
            .removeClass('level-' + (level + 1) + '-active');
       e.preventDefault();
-    }); 
+    }
+
+    $('[data-menu-back]').click(function(e) {
+      menuClick($(this), 'menu-back', e);
+    });
+
+    $('[data-menu-forward]').click(function(e) {
+      menuClick($(this), 'menu-forward', e);
+    });
   }
 }
 })(jQuery);
